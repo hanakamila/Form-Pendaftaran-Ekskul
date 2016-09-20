fd3a726b8b6a389a00d226744b115fc9055fca67
Hana Kamila <hanakamila17@gmail.com>
2016-09-20 10:04:29 +0700

    second commit:layout

--- app/src/main/java/id/sch/smktelkom_mlg/learn/formpendaftaranekskul/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/formpendaftaranekskul/MainActivity.java
@@ -2,8 +2,11 @@ package id.sch.smktelkom_mlg.learn.formpendaftaranekskul;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
